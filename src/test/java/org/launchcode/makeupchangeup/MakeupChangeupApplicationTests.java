a43bdd667566632e8a5b6b2e1a38408834401028
imSarahLaunch <syoungae@gmail.com>
2020-03-09 17:16:21 -0500

    started project for liftoff

---
+++ src/test/java/org/launchcode/makeupchangeup/MakeupChangeupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.makeupchangeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeupChangeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
